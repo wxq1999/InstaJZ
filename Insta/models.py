ace61ae4f9ec9a50aeeb998880902a7062296279
wangxiaoqi <987814762@qq.com>
2020-02-27 19:44:47 +0800

    Finish website development

--- Insta/models.py
+++ Insta/models.py
@@ -1,8 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 from imagekit.models import ProcessedImageField
 
+from django.urls import reverse
+
 # Create your models here.
+class InstaUser(AbstractUser):
+    profile_pic = ProcessedImageField(
+        upload_to = 'static/images/profiles',
+        format = 'JPEG',
+        options = {'quality':100},
+        blank = True,
+        null = True,
+        )
+
+    def get_connections(self):
+        connections = UserConnection.objects.filter(creator=self)
+        return connections
+
+    def get_followers(self):
+        followers = UserConnection.objects.filter(following=self)
+        return followers
+
+    def is_followed_by(self, user):
+        followers = UserConnection.objects.filter(following=self)
+        return followers.filter(creator=user).exists()
+
+    # def get_absolute_url(self):
+    #     return reverse('profile', args=[str(self.id)])
+
+    # def __str__(self):
+    #     return self.username
+
+class UserConnection(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    creator = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="friendship_creator_set")
+    following = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="friend_set")
+
+    def __str__(self):
+        return self.creator.username + ' follows ' + self.following.username
+
 class Post(models.Model):
+    author = models.ForeignKey(
+        InstaUser,
+        on_delete = models.CASCADE,
+        related_name = 'my_posts'
+    )
     titel = models.TextField(blank=True, null=True)
     image = ProcessedImageField(
         upload_to = 'static/images/posts',
@@ -11,3 +61,28 @@ class Post(models.Model):
         blank = True,
         null = True,
         )
+    
+    def get_absolute_url(self):
+        # return reverse("helloworld")
+        return reverse("post_detail", args=[str(self.id)])
+
+    def get_like_count(self):
+        return self.likes.count()
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete = models.CASCADE,
+        related_name = 'likes'
+    )
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete = models.CASCADE,
+        related_name = 'likes'
+    )
+
+    class Meta:
+        unique_together = ("post", "user")
+    
+    def __str__(self):
+        return 'Like: '+self.user.username+' likes '+self.post.titel
--- InstaJZ/settings.py
+++ InstaJZ/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Insta',
     'imagekit',
+    'annoying',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'posts'
+
+LOGOUT_REDIRECT_URL = 'posts'
+
+AUTH_USER_MODEL = 'Insta.InstaUser'
